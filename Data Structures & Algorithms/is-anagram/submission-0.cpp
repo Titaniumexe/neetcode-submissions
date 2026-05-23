4f88522d435a6b53645313a9d06cd387d0e0b8e6
Titaniumexe <shruthivkrishnan1@gmail.com>
2026-05-23 12:46:17 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length()!= t.length()){
+            return false;
+        }
+        unordered_map<char, int> hash1;
+        unordered_map<char,int> hash2;
+        for (int i=0;i<s.length();i++){
+            hash1[s[i]]++;
+            hash2[t[i]]++;
+        }
+
+        return hash1==hash2;
+
+    }
+};
